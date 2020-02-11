738820febad55b4dd4e572e0fe2f7d2675f18ec0
MarilynWaldman <mdwaldman22@hotmail.com>
2020-02-11 10:38:55 -0700

    age list thing

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -4,23 +4,30 @@ import { Text, StyleSheet, View, FlatList } from "react-native";
 const ListScreen = () => {
 
   const friends = [
-       {name: "doggie 1"},
-       {name: "doggie 2"},
-       {name: "doggie 3"},
-       {name: "doggie 4"},
-       {name: "doggie 5"},
+       {name: "doggie 1", age: 10},
+       {name: "doggie 2", age: 12},
+       {name: "doggie 3",  age: 10},
+       {name: "doggie 4", age: 10},
+       {name: "doggie 5", age: 10},
   ];
   return (
        <FlatList
        keyExtractor={friend => friend.name}
        data={friends}
        renderItem={({item}) =>{
-          return <Text>{ item.name}</Text>;
+          return <Text style={styles.textStyle}>
+          {item.name} - Age {item.age}
+          </Text>;
        }}
        />
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  textStyle: {
+    marginVertical: 50
+  }
+});
+
 
 export default ListScreen;
