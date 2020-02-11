35ec7e296321213fc69434e48756639aa4ebbb56
MarilynWaldman <mdwaldman22@hotmail.com>
2020-02-11 16:58:47 -0700

    reusable components

---
+++ src/components/ImageDetail.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, StyleSheet, View } from "react-native";
+
+const ImageDetail = () => {
+
+   return (
+        <Text> Image Detail Screen </Text>
+   )
+
+};
+
+const styles = StyleSheet.create({});
+
+export default ImageDetail;
---
+++ src/screens/ImageScreen.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Text, StyleSheet, View } from "react-native";
+import ImageDetail from "../components/ImageDetail.js"
+
+const ImageScreen = () => {
+
+   return (
+        <View>
+           <ImageDetail />
+           <ImageDetail />
+           <ImageDetail />
+        </View>
+   )
+
+};
+
+const styles = StyleSheet.create({});
+
+export default ImageScreen;
